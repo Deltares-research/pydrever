c0f90e7b3af8c386e233c344d5079ef900df8fa0
DIRECTORY\geer <pieter.vangeer@deltares.nl>
2024-01-02 13:11:31 +0100

    Add copyright notice

--- src/dikernelinputparser.py
+++ src/dikernelinputparser.py
@@ -1,3 +1,25 @@
+"""
+ Copyright (C) Stichting Deltares 2023-2024. All rights reserved.
+ 
+ This file is part of the dikernel-python toolbox.
+ 
+ This program is free software; you can redistribute it and/or modify it under the terms of
+ the GNU Lesser General Public License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License along with this
+ program; if not, see <https://www.gnu.org/licenses/>.
+ 
+ All names, logos, and references to "Deltares" are registered trademarks of Stichting
+ Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+ 
+ This is a license template.
+"""
+
 from dikernelinput import DikernelInput, HydraulicConditions, DikeSchematization
 from dikerneloutputlocations import (
     AsphaltOutputLocationSpecification,
